32c2e4b34e61bd895ce1418b06d08cc643c098df
SamX23 <skalexsong@gmail.com>
2022-02-06 15:03:33 +0700

    Update : finish basic slicing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,33 +4,37 @@ import Head from "next/head";
 import "../styles/home.css";
 import "../styles/utilities.css";
 import "../styles/detail.css";
+import "../styles/checkout.css";
+import "../styles/checkoutComplete.css";
+import "../styles/login.css";
+import "../styles/register.css";
+import "../styles/onboarding.css";
+import "../styles/404.css";
+import "../styles/member.css";
+import "../styles/profile.css";
+import "../styles/transactions.css";
+import "../styles/transaction-detail.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* <!-- Bootstrap CSS --> */}
+        <html lang="en" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
           crossOrigin="anonymous"
         />
-
-        {/* <!-- Google Font --> */}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
-
-        {/* <!-- AOS Animation --> */}
         <link
           href="https://unpkg.com/aos@2.3.1/dist/aos.css"
           rel="stylesheet"
         />
-
-        {/* <!-- Bootstrap --> */}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
