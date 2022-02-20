ddc6fe4e50f9be3dd8520caf8e58e9aa6abc4973
SamX23 <skalexsong@gmail.com>
2022-02-20 15:10:43 +0700

    Refactor : transaction component

--- components/organisms/TransactionSection/index.tsx
+++ components/organisms/TransactionSection/index.tsx
@@ -1,6 +1,6 @@
 import TransactionStep from "../../molecule/TransactionStep";
 
-const Feature = () => (
+const Transaction = () => (
   <section id="feature" className="feature pt-50 pb-50">
     <div className="container-fluid">
       <h2 className="text-4xl fw-bold color-palette-1 text-center mb-30">
@@ -27,4 +27,4 @@ const Feature = () => (
   </section>
 );
 
-export default Feature;
+export default Transaction;
