0ea72841d08638cdf01bf9bd78d35063cc08cab3
SamX23 <skalexsong@gmail.com>
2022-02-21 08:17:34 +0700

    Update : story section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ import Header from "../components/organisms/MainBanner";
 import Transaction from "../components/organisms/TransactionSection";
 import FeatureGame from "../components/organisms/FeatureGame";
 import Link from "next/link";
-import Image from "next/image";
 import ReachSection from "../components/organisms/ReachSection";
+import StorySection from "../components/organisms/StorySection";
 
 const Home: NextPage = () => {
   useEffect(() => AOS.init(), []);
@@ -19,49 +19,7 @@ const Home: NextPage = () => {
       <Transaction />
       <FeatureGame />
       <ReachSection />
-
-      <section className="story pt-50 pb-50">
-        <div className="container-xxl container-fluid">
-          <div className="row align-items-center px-lg-5 mx-auto gap-lg-0 gap-4">
-            <div
-              className="col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60"
-              data-aos="zoom-in"
-            >
-              <Image
-                src="/img/Header-9.png"
-                width={612}
-                height={452}
-                className="img-fluid"
-                alt=""
-              />
-            </div>
-            <div className="col-lg-5 col-12 ps-lg-60">
-              <div className="">
-                <h2 className="text-4xl fw-bold color-palette-1 mb-30">
-                  Win the battle.
-                  <br /> Be the Champion.
-                </h2>
-                <p className="text-lg color-palette-1 mb-30">
-                  Kami menyediakan jutaan cara untuk
-                  <br className="d-sm-block d-none" />
-                  membantu players menjadi
-                  <br className="d-sm-block d-none" /> pemenang sejati
-                </p>
-                <div className="d-md-block d-flex flex-column w-100">
-                  <Link href="/">
-                    <a
-                      className="btn btn-read text-lg rounded-pill"
-                      role="button"
-                    >
-                      Read Story
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <StorySection />
 
       <section className="footer pt-50">
         <footer>
