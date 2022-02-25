8b1461a6cc699e6ed2e63fee18d3dc2b43fe4fa7
SamX23 <skalexsong@gmail.com>
2022-02-25 07:50:45 +0700

    Update : fix bootstrap dependency undetecte

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Head from "next/head";
 import "../styles/utilities.css";
 import "../styles/home.css";
@@ -23,6 +23,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>StoreGG</title>
       </Head>
       <Component {...pageProps} />
+
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossOrigin="anonymous"
+      ></Script>
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-
 type Props = {};
 
 class MyDocument extends NextDocument<Props> {
@@ -15,6 +14,12 @@ class MyDocument extends NextDocument<Props> {
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
+          />
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
