d48ec9897490eef2ad042561256893cf90d69549
SamX23 <skalexsong@gmail.com>
2022-03-04 21:40:05 +0700

    Refactor : fix typo

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -2,7 +2,7 @@ import MemberOverview from "../../components/organisms/MemberOverview";
 import Sidebar from "../../components/organisms/SidebarNavigation";
 
 const Overview = () => (
-  <section className="Overview overflow-auto">
+  <section className="overview overflow-auto">
     <Sidebar />
     <MemberOverview />
   </section>
